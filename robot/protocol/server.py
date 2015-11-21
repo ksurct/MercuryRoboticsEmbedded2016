72bc22815cb47ea7a67356640c876abfc019b07e
Aaron Schif <aaronschif@gmail.com>
2015-11-20 23:50:12 -0600

    Update protobuf

--- robot/protocol/server.py
+++ robot/protocol/server.py
@@ -1,12 +1,21 @@
+import asyncio
+
 from .proto import main_pb2
 
 
 class Server(object):
-    def __init__(self, port):
+    def __init__(self, port, loop=None):
         self.port = port
+        self.loop = asyncio.get_event_loop() if loop is None else loop
 
     def __enter__(self):
         return self
 
     def __exit__(self, *enc):
         pass
+
+    def parse_message(self, msg):
+        return main_pb2.Robot.ParseFromString(msg)
+
+    def serialize_message(self, msg):
+        return main_pb2.BaseStation.SerializeToString()
