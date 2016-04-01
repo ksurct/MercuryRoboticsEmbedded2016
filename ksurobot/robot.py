7babc6d92fc144d2474e9e1a650342c4696e4377
Aaron Schif <aaronschif@gmail.com>
2016-03-31 20:16:00 -0500

    Update pins

--- ksurobot/robot.py
+++ ksurobot/robot.py
@@ -21,18 +21,18 @@ from .util import AsyncioLoop, get_config
 class Robot(RobotBase):
     def __init__(self):
         super().__init__()
-        self.head_lights = self.attach_device(LED(6))
-        self.motor_left = self.attach_device(Motor(20, 19, 18))
-        self.motor_left_speed = self.attach_device(SpeedEncoder(22, 21))
-        self.motor_right = self.attach_device(Motor(17, 16, 13))
-        self.motor_right_speed = self.attach_device(SpeedEncoder(24, 23))
-        self.camera = self.attach_device(Servo(0, 0))
-        self.launch = self.attach_device(Servo(0, 0))
-        self.claw  = self.attach_device(Servo(0, 0))
-        self.dist_fr = self.attach_device(DistanceSensor(0))
-        self.dist_fl= self.attach_device(DistanceSensor(0))
-        self.dist_br = self.attach_device(DistanceSensor(0))
-        self.dist_bl = self.attach_device(DistanceSensor(0))
+        self.head_lights = self.attach_device(LED(5))
+        self.motor_left = self.attach_device(Motor(6, 13, 19))
+        self.motor_left_speed = self.attach_device(SpeedEncoder(8, 25))
+        self.motor_right = self.attach_device(Motor(24, 23, 18))
+        self.motor_right_speed = self.attach_device(SpeedEncoder(17, 27))
+        # self.camera = self.attach_device(Servo(0, 0))
+        # self.launch = self.attach_device(Servo(0, 0))
+        # self.claw  = self.attach_device(Servo(0, 0))
+        # self.dist_fr = self.attach_device(DistanceSensor(0))
+        # self.dist_fl= self.attach_device(DistanceSensor(0))
+        # self.dist_br = self.attach_device(DistanceSensor(0))
+        # self.dist_bl = self.attach_device(DistanceSensor(0))
 
 
 def main():
