0294169bcb7b039484adb53d513a3b058f96a3b9
Aaron Schif <aaronschif@gmail.com>
2016-04-07 18:43:24 -0500

    Add pram

--- ksurobot/test/interact.py
+++ ksurobot/test/interact.py
@@ -8,12 +8,12 @@ from ..protocol.proto import main_pb2
 from ..robot import Robot
 
 
-def set_motor(v):
+def set_motor(r, v):
     r.motor_right.set(v)
     r.motor_left.set(v)
 
 
-def set_motor_rpm(v):
+def set_motor_rpm(r, v):
     r.motor_left_rpm.set(v)
     r.motor_left_rpm.set(v)
 
