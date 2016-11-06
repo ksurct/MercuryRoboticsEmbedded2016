0cdd0a033cf820485d5eb21df984cbd8f6046cdc
Aaron Schif <aschif@netdevgroup.com>
2016-11-06 11:08:59 -0600

    Video piped in from raspivid

--- ksurobot/util/video_stream.py
+++ ksurobot/util/video_stream.py
@@ -1,7 +1,10 @@
 import subprocess
+import shlex
 import atexit
 
 VIDEO_STRING = "gst-launch-1.0 v4l2src device=/dev/video0 ! 'video/x-raw,width=640,height=480' !  x264enc pass=qual quantizer=2 tune=zerolatency ! rtph264pay ! udpsink host={} port={}"
+VIDEO_STRING = """bash -c "raspivid -o - -t 0 | gst-launch-1.0 fdsrc ! h264parse !  rtph264pay config-interval=1 ! udpsink host=10.243.247.87 port=9001" """
+
 
 class VideoHandler(object):
     def __init__(self):
@@ -9,7 +12,7 @@ class VideoHandler(object):
 
     def open_video(self, host, port):
         if self.process_handle is None:
-            self.process_handle = subprocess.run(VIDEO_STRING.format(host, port), shell=True)
+            self.process_handle = subprocess.Popen(shlex.split(VIDEO_STRING.format(host, port)))
 
     def close_video(self):
         if self.process_handle is not None:
